ce32e4ea671c43a96917d68dcc7f38051f79c2f0
Taylor Daugherty <taylor.daugherty@bettercloud.com>
2017-12-07 16:15:10 -0500

    holding profile

--- services/client/src/main/java/com/bettercloud/spring/cloud/controller/ClientController.java
+++ services/client/src/main/java/com/bettercloud/spring/cloud/controller/ClientController.java
@@ -1,35 +1,29 @@
 package com.bettercloud.spring.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 
-@RestController("/client")
+@RestController
+@RequestMapping("/client")
 public class ClientController {
 
-    private DiscoveryClient discoveryClient;
+    private String serviceEndpoint;
     private RestTemplate restTemplate;
 
     @Autowired
-    public ClientController(final DiscoveryClient discoveryClient, final RestTemplate restTemplate) {
-        this.discoveryClient = discoveryClient;
+    public ClientController(@Value("${service.me}") final String serviceEndpoint, final RestTemplate restTemplate) {
+        this.serviceEndpoint = serviceEndpoint;
         this.restTemplate = restTemplate;
-    }
-
-    @GetMapping("/services/{applicationName}")
-    public List<ServiceInstance> serviceInstances(@PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
     }
 
     @GetMapping
     public String getService() {
-        return restTemplate.getForEntity("http://service/me", String.class).getBody();
+        return restTemplate.getForEntity(serviceEndpoint, String.class).getBody();
     }
 
 }
